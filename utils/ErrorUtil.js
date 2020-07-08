520b0c0c451981d0bc6c2054e992500d542930e5
sekfung <sekfung.lau@gmail.com>
2020-07-08 14:36:41 +0800

    filter unnecessary error

--- utils/ErrorUtil.js
+++ utils/ErrorUtil.js
@@ -1,8 +1,13 @@
 import RNProvider from '../RNProvider'
+import {D} from 'esecubit-wallet-sdk'
 import {errorMap} from 'esecubit-wallet-i18n/errorMap'
 
 class ErrorUtil {
   static getErrorMsg(errCode) {
+    // filter unnecessary error
+    if (errCode === D.error.offlineModeNotAllowed) {
+      return
+    }
     if (errorMap[errCode] === undefined) {
       console.warn('getErrorMsg,undefined', errCode)
       return RNProvider.I18n.t(errorMap[10001])
